2850f760fc25d97237b76e0c1f0308951088c745
jaehyungPark <06077r@gmail.com>
2021-02-03 02:19:34 +0900

    20210202/programmers/lv1_start

---
+++ java1/programmers/kNumber.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for(int i = 0; i < commands.length; i++) {
+        	
+        }
+        return answer;
+    }
+}
+
+public class kNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
