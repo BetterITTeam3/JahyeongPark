b0a662c0a2e4ab60e1fffed6235b2fc5917deaea
jaehyungPark <06077r@gmail.com>
2021-02-03 20:21:42 +0900

    20210203/programmers/lv1/kNumber

--- java1/programmers/kNumber.java
+++ java1/programmers/kNumber.java
@@ -1,17 +1,34 @@
+import java.util.Arrays;
+
 class Solution {
+    
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
+    	int[] answer = new int[commands.length];
         for(int i = 0; i < commands.length; i++) {
-        	
+        	int[] box = new int[ commands[i][1] - commands[i][0] + 1 ]; // box: 정렬을 위한 배열을 담을 곳 
+        	for(int j = 0; j < box.length; j++) {
+        		box[j] = array[ j + commands[i][0] - 1 ];
+        	}
+        	 Arrays.sort(box);
+            answer[i] = box[ commands[i][2] - 1 ];
         }
         return answer;
     }
+    
 }
 
 public class kNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] arr = {1, 5, 2, 6, 3, 7, 4};
+		int[][] com = {
+				{2, 5, 3},
+				{4, 4, 1},
+				{1, 7, 3} };
+		
+		Solution s1 = new Solution();
+		
+		System.out.println(Arrays.toString(s1.solution(arr, com)));
 
 	}
 
